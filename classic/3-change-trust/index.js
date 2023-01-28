91e6e08c0585239d4f4022b9d14a49f164778481
František Poboček <pobocekfrantisek@gmail.com>
2023-01-28 08:14:23 +0100

    Change trust

--- classic/3-change-trust/index.js
+++ classic/3-change-trust/index.js
@@ -8,6 +8,7 @@ const {
   Asset,
   BASE_FEE,
 } = require("stellar-sdk");
+
 const { config } = require("dotenv");
 
 config();
@@ -45,13 +46,23 @@ const fundAddress = async (keypair, qAccount, qKeypair) => {
 };
 
 const main = async () => {
-  // We need two keypairs for this transaction: a source account, and a destination account. In this case, we need both to be funded on the testnet.
+  // We'll need two keypairs for this transaction: our quest keypair (the one trusting the asset), and an issuer keypair (the one issuing the asset).
   const questKeypair = Keypair.fromSecret(process.env.SK);
-  const destinationKeypair = Keypair.random();
+  const issuerKeypair = Keypair.random();
 
   // We set up the server and account that will be used to build and submit the transaction.
   const questAccount = await server.loadAccount(questKeypair.publicKey());
-  await fundAddress(destinationKeypair, questAccount, questKeypair);
+  await fundAddress(issuerKeypair, questAccount, questKeypair);
+
+  // We'll need to create an asset for our quest account to trust. A few quick notes about assets:
+  // Assets can exist in three forms: alphanumeric 4, alphanumeric 12, and liquidity pool shares.
+  // Alphanumeric asset codes must be less than or equal to 4 or 12 characters, respectively.
+  // Asset codes are case-sensitive, so Pizza, PIZZA, and pizza are all different assets.
+  // The issuer is the public key of the account that is issuing the asset. The issuing account can't hold a balance of its own asset. The asset's issuer field determines who you are trusting to send you this particular asset.
+  const santaAsset = new Asset(
+    (code = "SANTA"),
+    (issuer = issuerKeypair.publicKey())
+  );
 
   // Build the transaction
   const transaction = new TransactionBuilder(questAccount, {
@@ -59,10 +70,10 @@ const main = async () => {
     networkPassphrase: Networks.TESTNET,
   })
     .addOperation(
-      Operation.payment({
-        destination: destinationKeypair.publicKey(),
-        asset: Asset.native(),
-        amount: "100",
+      Operation.changeTrust({
+        asset: santaAsset,
+        limit: "100",
+        source: questKeypair.publicKey(),
       })
     )
     .setTimeout(30)
